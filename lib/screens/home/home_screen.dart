404e32c02bf44c83f9c576014acd82fabc871efe
Mola <huylaqn1008@gmail.com>
2023-02-28 22:06:30 +0700

    food-app

---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,282 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class HomeScreen extends StatelessWidget {
+  Widget singalProducts() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 5),
+      height: 230,
+      width: 180,
+      decoration: BoxDecoration(
+        color: Colors.grey[200],
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        children: [
+          Expanded(
+            flex: 2,
+            child: Image.network(
+                "https://toppng.com/uploads/preview/basil-pianta-11550720634vzutfgkl6x.png"),
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Fresh Basil",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  "50\$/50 Gram",
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        padding: EdgeInsets.only(left: 0),
+                        height: 30,
+                        width: 50,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Row(
+                          children: [
+                            Expanded(
+                                child: Text(
+                              "50 Gram",
+                              style: TextStyle(
+                                fontSize: 10,
+                              ),
+                            )),
+                            Expanded(
+                              child: Icon(
+                                Icons.arrow_drop_down,
+                                size: 20,
+                                color: Colors.yellow,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Expanded(
+                      child: Container(
+                        height: 30,
+                        width: 50,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.remove,
+                              size: 15,
+                              color: Color(0xffd0b84c),
+                            ),
+                            Text(
+                              "1",
+                              style: TextStyle(
+                                color: Color(0xffd0b84c),
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Icon(
+                              Icons.add,
+                              size: 15,
+                              color: Color(0xffd0b84c),
+                            )
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffcbcbcb),
+      drawer: Drawer(),
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.black),
+        title: Text(
+          "Home",
+          style: TextStyle(color: Colors.black, fontSize: 17),
+        ),
+        actions: [
+          CircleAvatar(
+            radius: 12,
+            backgroundColor: Color(0xffd4d181),
+            child: Icon(
+              Icons.search,
+              size: 15,
+              color: Colors.black,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 5),
+            child: CircleAvatar(
+              backgroundColor: Color(0xffd4d181),
+              radius: 12,
+              child: Icon(
+                Icons.shop,
+                size: 17,
+                color: Colors.black,
+              ),
+            ),
+          )
+        ],
+        backgroundColor: Color(0xffd6b738),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+        child: ListView(
+          children: [
+            Container(
+              height: 150,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: NetworkImage(
+                        "https://image.shutterstock.com/image-vector/collection-various-fresh-vegetables-isolated-260nw-2065382336.jpg")),
+                color: Colors.red,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding:
+                                const EdgeInsets.only(right: 130, bottom: 10),
+                            child: Container(
+                              height: 50,
+                              width: 50,
+                              decoration: BoxDecoration(
+                                color: Color(0xffd1ad17),
+                                borderRadius: BorderRadius.only(
+                                    bottomRight: Radius.circular(50),
+                                    bottomLeft: Radius.circular(30)),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  "HKLP",
+                                  style: TextStyle(
+                                      fontSize: 17,
+                                      color: Colors.white,
+                                      shadows: [
+                                        BoxShadow(
+                                          color: Colors.green,
+                                          blurRadius: 10,
+                                          offset: Offset(3, 3),
+                                        )
+                                      ]),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Text(
+                            "30% Off",
+                            style: TextStyle(
+                              fontSize: 40,
+                              color: Colors.green[100],
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 20),
+                            child: Text(
+                              "On all vegetables products",
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 5),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Herbs Seasonings"),
+                  Text(
+                    "View all",
+                    style: TextStyle(color: Colors.grey),
+                  ),
+                ],
+              ),
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  singalProducts(),
+                  singalProducts(),
+                  singalProducts(),
+                  singalProducts(),
+                  singalProducts(),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 5),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Fresh Fruits"),
+                  Text(
+                    "View all",
+                    style: TextStyle(color: Colors.grey),
+                  ),
+                ],
+              ),
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  singalProducts(),
+                  singalProducts(),
+                  singalProducts(),
+                  singalProducts(),
+                  singalProducts(),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
